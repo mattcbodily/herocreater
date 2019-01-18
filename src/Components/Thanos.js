b15b2572bb98a251663339c2c53884cb80e07b7e
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-01-18 16:30:27 -0700

    added to the handleHealth function in Thanos.js to also adjust the healthMessage property

--- src/Components/Thanos.js
+++ src/Components/Thanos.js
@@ -14,9 +14,32 @@ class Thanos extends Component {
     handleThanosHealth(){
         const damage = this.props.numOfHeroes.length;
         this.setState({health: 16 - damage})
+        if (this.state.health >= 10){
+            this.setState({healthMessage: 'Thanos is strong! Add heroes to defeat him!'})
+        } else if (this.state.health < 10 && this.state.health >= 7) {
+            this.setState({healthMessage: `That's it! Keep going!`})
+        } else if (this.state.health < 7 && this.state.health > 1) {
+            this.setState({healthMessage: 'Almost There!'})
+        } else if (this.state.health === 1) {
+            this.setState({healthMessage: 'Finish Him!'})
+        } else {
+            this.setState({healthMessage: 'Defeated!'})
+        }
     }
 
-    
+    //handleHealthMessage(){
+    //   if (this.state.health >= 10){
+    //        this.setState({healthMessage: 'Thanos is strong! Add heroes to defeat him!'})
+    //    } else if (this.state.health < 10 && this.state.health >= 7) {
+    //        this.setState({healthMessage: `That's it! Keep going!`})
+    //    } else if (this.state.health < 7 && this.state.health > 1) {
+    //        this.setState({healthMessage: 'Almost There!'})
+    //    } else if (this.state.health === 1) {
+    //        this.setState({healthMessage: 'Finish Him!'})
+    //    } else {
+    //        this.setState({healthMessage: 'Defeated!'})
+    //    }
+    //}
     
     render(){
         console.log(this.props.numOfHeroes)
